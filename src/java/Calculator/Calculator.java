ca99e04599301e5ac9a89bb5e9b30b3447cb8f2d
lamnh3 <huylam0304@gmail.com>
2024-09-09 19:03:52 +0700

    add validate
    must input digit

--- src/java/Calculator/Calculator.java
+++ src/java/Calculator/Calculator.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package Calculator;
 
 import java.io.IOException;
@@ -17,36 +16,39 @@ import jakarta.servlet.http.HttpServletResponse;
  *
  * @author lam
  */
-@WebServlet(name="Calculator", urlPatterns={"/cal"})
+@WebServlet(name = "Calculator", urlPatterns = {"/cal"})
 public class Calculator extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet Calculator</title>");  
+            out.println("<title>Servlet Calculator</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet Calculator at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet Calculator at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -54,19 +56,19 @@ public class Calculator extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        int a =  Integer.parseInt(request.getParameter("a"))   ;
-        int b =  Integer.parseInt(request.getParameter("b"))   ;
+            throws ServletException, IOException {
+        int a = Integer.parseInt(request.getParameter("a"));
+        int b = Integer.parseInt(request.getParameter("b"));
         String op = request.getParameter("operator");
-       response.getWriter().print(a);
+        response.getWriter().print(a);
         response.getWriter().print(b);
-         response.getWriter().print(op);
-       
-        
-    } 
+        response.getWriter().print(op);
 
-    /** 
+    }
+
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -74,37 +76,46 @@ public class Calculator extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        double a =  Double.parseDouble(request.getParameter("a"))   ;
-        double b =  Double.parseDouble(request.getParameter("b"))   ;
+            throws ServletException, IOException {
+        String aValue = request.getParameter("a");
+        String bValue = request.getParameter("b");
+        double a, b;
         String op = request.getParameter("operator");
         double result;
         String s;
-        switch (op) {
-            case "add":
-                    s="+";
-                    result= a+ b;
-                break;
-            case "sub":
-                    s="-";
-                    result= a- b;
-                break; 
-            case "mul":
-                    s="*";
-                    result= a* b;
-                break;  
-             case "div":
-                    s="/";
-                    result= a/ b;
-                break;   
-            default:
-                throw new AssertionError();
+        try {
+            a = Double.parseDouble(aValue);
+            b = Double.parseDouble(bValue);
+            switch (op) {
+                case "add":
+                    s = "+";
+                    result = a + b;
+                    break;
+                case "sub":
+                    s = "-";
+                    result = a - b;
+                    break;
+                case "mul":
+                    s = "*";
+                    result = a * b;
+                    break;
+                case "div":
+                    s = "/";
+                    result = a / b;
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+            response.getWriter().print(a + " " + s + " " + b + " = " + result);
+
+        } catch (NumberFormatException e) {
+            response.getWriter().print("<h1>Input must be digit.</h1>");
         }
-       response.getWriter().print(a+" "+s+" "+b+" = "+result);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
